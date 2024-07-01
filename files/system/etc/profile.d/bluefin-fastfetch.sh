4f46bda8993918d275f5fd951d449790c927bc81
David Personette <dperson@gmail.com>
2024-07-01 16:41:35 -0400

    chore: Customizations

---
+++ files/system/etc/profile.d/bluefin-fastfetch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env -S sh
+
+LOGO="$(find /usr/share/ublue-os/bluefin-logos/symbols/ | shuf -n 1)"
+
+#shellcheck disable=SC2139
+alias fastfetch="fastfetch --logo ${LOGO} -c /usr/share/ublue-os/ublue-os.jsonc"
+alias neofetch=fastfetch
+unset LOGO
---
+++ files/system/etc/profile.d/open.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env -S sh
+alias open="xdg-open &>/dev/null"
