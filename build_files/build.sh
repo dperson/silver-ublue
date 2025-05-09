00bc62610ed12649985740cbc92e5811cde2308b
David Personette <dperson@gmail.com>
2025-05-08 21:37:34 -0400

    chore: Customizations

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,24 +1,65 @@
-#!/bin/bash
+#!/usr/bin/env -S bash
 
-set -ouex pipefail
+set -euxo pipefail
 
-### Install packages
+mkdir -p /var/roothome
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# We need to remove this package here because lots of local files
+dnf remove -y ublue-os-luks ublue-os-just ublue-os-udev-rules ublue-os-signing \
+      ublue-os-update-services
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+echo "::group:: Copy Files"
+# Copy ISO list for `install-system-flaptaks`
+install -Dm0644 -t /etc/ublue-os/ /ctx/build_files/iso/*.list
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# Copy Files to Container
+rsync -Krtv /ctx/brew/ /
+rsync -Krtv /ctx/system_files/ /
 
-#### Example for enabling a System Unit File
+mkdir -p /tmp/scripts/helpers
+install -Dm0755 /ctx/build_files/utils/ghcurl /tmp/scripts/helpers/ghcurl
+export PATH="/tmp/scripts/helpers:$PATH"
+echo "::endgroup::"
 
-systemctl enable podman.socket
+# Generate image-info.json
+/ctx/build_files/base/00-image-info.sh
+
+# Install Kernel and Akmods
+/ctx/build_files/base/03-install-kernel-akmods.sh
+
+# Install Additional Packages
+/ctx/build_files/base/04-packages.sh
+
+# Install Overrides and Fetch Install
+/ctx/build_files/base/05-override-install.sh
+
+## late stage changes
+
+# Install fonts
+/ctx/build_files/base/12-fonts.sh
+
+# Setup quadlets
+/ctx/build_files/base/13-quadlets.sh
+
+# Systemd and Remove Items
+/ctx/build_files/base/17-cleanup.sh
+
+# Run workarounds for lf (Likely not needed)
+/ctx/build_files/base/18-workarounds.sh
+
+# Regenerate initramfs
+/ctx/build_files/base/19-initramfs.sh
+
+# Validate all repos are disabled before committing
+/ctx/build_files/validate-repos.sh
+
+# Clean Up
+echo "::group:: Cleanup"
+/ctx/build_files/clean-stage.sh
+mkdir -p /var/tmp && chmod -R 1777 /var/tmp
+
+ostree container commit
+echo "::endgroup::"
+
+# Simple Tests
+/ctx/build_files/base/20-tests.sh
---
+++ system_files/etc/profile.d/ramalama.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env -S bash
+if command -v ramalama >/dev/null; then
+  alias rl="ramalama"
+fi
